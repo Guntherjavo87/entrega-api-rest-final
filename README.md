f8255965018846c6dafeabb024cc1b30a7fb1d66
Guntherjavo87 <gunther7985@gmail.com>
2025-07-24 15:04:37 -0300

    firebase listo

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # entrega-api-rest-final
+
+
+API REST para gestión de productos desarrollada con Node.js y Express.
+
+1. Instalación
+
+Clonar el repositorio
+
+2. Instalar dependencias:
+
+npm install
+
+3.Configurar variables de entorno:
+
+# Copiar el archivo de ejemplo y completar los datos requeridos
+
+cp .env-example .env
+
+Luego editar el archivo .env con los valores correspondientes para tu entorno.
+
+4.Ejecutar en modo desarrollo:
+
+npm run dev
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+
+import "dotenv/config";
 import express from "express";
+import cors from 'cors';
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({ message: "Hola Javier API Rest en Node.js" });
-  });
 
-const PORT = 3000
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Rest en Node.js" });
+});
+
+import productsRouter from "./src/routes/products.router.js";
+app.use("/api", productsRouter);
+
+import authRouter from "./src/routes/auth.router.js";
+app.use(authRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Not Found" });
+});
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
